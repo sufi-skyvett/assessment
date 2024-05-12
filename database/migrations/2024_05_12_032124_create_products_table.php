2c16794709ca94cfed78765b600900a7e35ac6b5
Ahmad Sufi Bin Abdul Hamid <sufi@mindwave.my>
2024-05-13 00:15:21 +0800

    update migration

--- database/migrations/2024_05_12_032124_create_products_table.php
+++ database/migrations/2024_05_12_032124_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('price')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
             $table->string('details')->nullable();
             $table->boolean('is_published')->default(false);
             $table->timestamps();
